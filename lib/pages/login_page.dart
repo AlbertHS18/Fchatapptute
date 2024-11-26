9a95f76eff53ca8a6eeb1ea29028d804fe827ee1
Albert <albert182219@gmail.com>
2024-11-25 23:05:19 -0400

    Commit 1

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+@override
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -7,6 +9,36 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo
+          Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary
+          ),
+
+          const SizedBox(height: 50),
+
+        // welcome back message
+        Text(
+          "Welcome back, you' ve been missed!",
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+
+        const SizedBox(height: 50),
+        // email textfield
+        //pw textfield
+        //login button
+        // register now
+
+      ],
+      ),
+    ),
     );
   }
 }
