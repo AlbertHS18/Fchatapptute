fec2e5f55524e4f79092a65c50cd0f8fa6cc6f57
Albert <albert182219@gmail.com>
2024-12-01 19:21:56 -0400

    Commit m#3

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,15 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fchatapptute/components/my_textfield.dart';
 import 'package:fchatapptute/services/auth/auth_service.dart';
 import 'package:fchatapptute/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget{
   final String receiverEmail;
+  final String receiverID;
 
 
 
     ChatPage({
       super.key, 
-      required this.receiverEmail, required receiverID,
+      required this.receiverEmail, 
+      required this.receiverID
     });
 
     final TextEditingController _messageController = TextEditingController();
@@ -21,20 +25,77 @@ class ChatPage extends StatelessWidget{
     void sendMessage() async {
       if (_messageController.text.isNotEmpty) {
         await _chatService.sendMessage(receiverID, _messageController.text);
-      
 
-      }
+
+          // clear text controller
+        _messageController.clear();
+      } 
 
     }
-    
 
-      
       @override
       Widget build(BuildContext context) {
         return Scaffold(
             appBar: AppBar(title: Text(receiverEmail)),
-      
+            body: Column(
+              children: [
+                // display all messages
+                  Expanded(
+                    child: _buildMessageList(),
+                 ), // Expanded
+              ],
+            ),
         );
       }
 
-}
+      Widget _buildMessageList() {
+        String senderID = _authService.getCurrentUser()!.uid;
+        return StreamBuilder(
+          stream: _chatService.getMessages(receiverID, senderID),
+          builder: (context, shapshot) {
+            if (shapshot.hasError) {
+              return const Text("Error");
+            }
+
+            if (shapshot.connectionState == ConnectionState.waiting){
+              return const Text("Loading..");
+            }
+
+            return ListView(
+                children:
+                   shapshot.data!.docs.map((doc) => _builMessageItem(doc)).toList(),
+            );
+          },
+        );  
+      }
+
+      Widget _builMessageItem(DocumentSnapshot doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+        return Text(data["message"]);
+      }
+            
+      Widget _builUserInput(){
+        return Row(
+          children: [
+
+            Expanded(
+              child:MyTextField(
+                controller: _messageController,
+                hintText: "Type a message",
+                obscureText: false,
+              ),
+           ),
+
+            IconButton(
+              onPressed: sendMessage,
+              icon: const Icon(Icons.arrow_upward),
+
+            ),
+
+          ],
+
+        );
+
+    }
+  }
--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -60,7 +60,7 @@ class ChatService{
                     .orderBy("timestamp", descending: false)
                     .snapshots();
 
-
+                    
               }
 
                       
