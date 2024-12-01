8ae80995656f454d6bf73e7a20b8ded10cfee847
Albert <albert182219@gmail.com>
2024-12-01 00:24:32 -0400

    Commit m#01

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,9 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fchatapptute/models/message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatService{
   
   // get instance of firestore
     final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+
 
   // get user stream
     Stream<List<Map<String, dynamic>>> getUsersStream(){
@@ -15,9 +19,37 @@ class ChatService{
 
         });
       }
-    }
-  // send message
+    
+      // send message
+            Future <void> sendMessage(String receiverID, message) async {
 
-  //get messages
+              final String currentUserID = _auth.currentUser!.uid;
+              final String currentUserEmail = _auth.currentUser!.email!;
+              final Timestamp timestamp = Timestamp.now();
+
+
+                Message newMessage = Message(
+                  senderID: currentUserEmail,
+                  senderEmail: currentUserID,
+                  receiverID: receiverID,
+                  message: message,
+                  timestamp: timestamp,
+                  );
+
+                  List<String> ids = [currentUserID, receiverID];
+                  ids.sort();
+                  String chatRoomID = ids.join('_');
+
+
+                  await _firestore
+                    .collection("chat_rooms")
+                    .doc(chatRoomID)
+                    .collection("messages")
+                    .add(newMessage.toMap());
+            }
+
+                      //get messages
+
+  }
 
 
